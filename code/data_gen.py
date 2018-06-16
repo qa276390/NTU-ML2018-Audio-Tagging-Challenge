02dbc6b00165e9fbf23307626f02fd3c4da174db
shihkaiho <xxxxx112358xxxxx@gmail.com>
2018-06-16 21:50:19 +0800

    new

--- code/data_gen.py
+++ code/data_gen.py
@@ -11,83 +11,13 @@ import numpy as np
 import IPython
 import IPython.display as ipd  # To play sound in the notebook
 
-
-# In[2]:
-
-
 SAMPLE_RATE = 44100
 train = pd.read_csv('../input/train.csv')
 train.head()
 
-
-# In[3]:
-
-
 def audio_norm(data):
     max_data = np.max(np.absolute(data))
     return data/(max_data+1e-6)*0.5
-
-
-# In[4]:
-
-
-wav, sampling_rate = librosa.core.load('../input/audio_train/'+train.fname[0], sr=16000)
-wav = audio_norm(wav)
-IPython.display.display(ipd.Audio('../input/audio_train/'+train.fname[0]))
-
-
-# In[5]:
-
-
-plt.plot(wav, '-', )
-
-
-# In[6]:
-
-
-sampling_rate
-
-
-# In[7]:
-
-
-#pitch shift
-bpo = 24 #how many steps per octave
-pr = 1 #pitch shift range
-ps = int(np.random.uniform(-pr * bpo, pr * bpo) + 0.5) #how many (fractional) half-steps to shift y
-shifted_wav = librosa.effects.pitch_shift(wav, sampling_rate, n_steps = ps, bins_per_octave = bpo)
-librosa.output.write_wav('../temp/temp.wav', shifted_wav, sampling_rate)
-IPython.display.display(ipd.Audio( '../temp/temp.wav'))
-
-
-# In[8]:
-
-
-#time stretch
-tr = 2.0 #speed up/down rate
-lgtr = np.log(tr)
-ts = 2 ** np.random.uniform(-lgtr,lgtr)
-shifted_wav = librosa.effects.time_stretch(wav, ts)
-librosa.output.write_wav('../temp/temp.wav', shifted_wav, sampling_rate)
-IPython.display.display(ipd.Audio( '../temp/temp.wav'))
-shifted_wav
-
-
-# In[9]:
-
-
-# white noise
-wnvr = 0.1 # white noise volume range
-wnv  = np.random.uniform(0, wnvr) # white noise volume, random
-shifted_wav = wav + np.random.uniform(-wnv, wnv, wav.shape)
-librosa.output.write_wav('../temp/temp.wav', shifted_wav, sampling_rate)
-IPython.display.display(ipd.Audio( '../temp/temp.wav'))
-print(shifted_wav.shape)
-print(wav.shape)
-
-
-# In[10]:
-
 
 class Config(object):
     def __init__(self,
@@ -109,10 +39,6 @@ class Config(object):
             self.dim = (self.n_mfcc, 1 + int(np.floor(self.audio_length/512)), 1)
         else:
             self.dim = (self.audio_length, 1)
-
-
-# In[11]:
-
 
 def prepare_data(df, config, data_dir):
     X = np.empty(shape=(df.shape[0] * config.datagen_num, config.dim[0], config.dim[1], 1))
@@ -158,20 +84,15 @@ def prepare_data(df, config, data_dir):
     return X, y
 
 
-# In[12]:
-
-
 config = Config(sampling_rate=44100, audio_duration=2, n_folds=5, 
-                learning_rate=0.001, use_mfcc=True, n_mfcc=40, datagen_num=2)
+                learning_rate=0.001, use_mfcc=True, n_mfcc=40, datagen_num=3)
 train = pd.read_csv("../input/train.csv")
 LABELS = list(train.label.unique())
 label_idx = {label: i for i, label in enumerate(LABELS)}
 train.set_index("fname", inplace=True)
 train["label_idx"] = train.label.apply(lambda x: label_idx[x])
 
-
-# In[13]:
-
-
 X_train, y = prepare_data(train, config, '../input/audio_train/')
 
+np.save('X_train.npy',X_train)
+np.save('y.npy',y)
